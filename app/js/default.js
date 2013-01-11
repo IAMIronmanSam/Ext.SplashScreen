ad66a42b926c5b185ae9d79639871c38bbd8b22f
Arivazhagan Ambigapathi <arivu86@gmail.com>
2013-01-11 13:47:25 +0530

    Re-Engg Code
    New Sample

---
+++ app/js/default.js
@@ -0,0 +1,44 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    var _splash,
+        _coordinates = { x: 0, y: 0, width: 0, height: 0 };
+
+
+    app.onactivated = function (args) {
+
+        if (args.detail.kind === activation.ActivationKind.launch) {
+
+            _splash = args.detail.splashScreen;
+            _coordinates = _splash.imageLocation;
+            ExtendedSplash.show(_splash);
+
+            window.addEventListener("resize", onResize, false);
+            _splash.addEventListener("dismissed", onSplashScreenDismissed, false);
+
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    function onSplashScreenDismissed() {
+
+        //document.querySelector("#learnMore").addEventListener("click", ExtendedSplash.remove, false);
+    }
+
+    function onResize() {
+        
+        if (_splash) {
+            _coordinates = _splash.imageLocation;
+            ExtendedSplash.updateImageLocation(_splash);
+        }
+    }
+
+    app.start();
+})();
---
+++ app/js/extendedSplash.js
@@ -0,0 +1,47 @@
+﻿
+(function () {
+    "use strict";
+
+    function show(splash) {
+        var extendedSplashImage = document.querySelector("#extendedSplashImage");
+        position(extendedSplashImage, splash);
+
+        WinJS.Utilities.removeClass(extendedSplashScreen, "hidden");
+    }
+
+    function remove() {
+        if (isVisible()) {
+
+            var extendedSplashScreen = document.querySelector("#extendedSplashScreen")
+            WinJS.Utilities.addClass(extendedSplashScreen, "hidden");
+        }
+    }
+
+    function updateImageLocation(splash) {
+
+        if (isVisible()) {
+            var extendedSplashImage = document.querySelector("#extendedSplashImage");
+
+            position(extendedSplashImage, splash);
+        }
+    }
+
+    function position(element, splash) {
+        element.style.top = splash.imageLocation.y + "px";
+        element.style.left = splash.imageLocation.x + "px";
+        element.style.height = splash.imageLocation.height + "px";
+        element.style.width = splash.imageLocation.width + "px";
+    }
+
+    function isVisible() {
+        var extendedSplashScreen = document.querySelector("#extendedSplashScreen");
+        return !(WinJS.Utilities.hasClass(extendedSplashScreen, "hidden"));
+    }
+
+    WinJS.Namespace.define("ExtendedSplash", {
+        show: show,
+        updateImageLocation: updateImageLocation,
+        isVisible: isVisible,
+        remove: remove
+    });
+})();
